72596c4081d10342e1c743f7c2c9ba2ef5df6f01
Godservant Emmanuel <137901775+lordemmag-devops@users.noreply.github.com>
2026-01-08 17:29:59 +0100

    added: login endpoint, JWT token generation, Auth middleware, One protected route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../config/db');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
 
 router.post('/register', async (req, res) => {
     try {
@@ -36,6 +38,51 @@ router.post('/register', async (req, res) => {
         console.error(error);
         res.status(500).json({ message: 'Server error' });
     }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const userResult = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+
+        if (userResult.rows.length === 0) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const user = userResult.rows[0];
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign(
+            { id: user.id, role: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        res.json({ token });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+router.get('/profile', auth, async (req, res) => {
+    const userResult = await pool.query(
+        "SELECT id, email, role FROM users WHERE id = $1",
+        [req.user.id]
+    );
+
+    res.json(userResult.rows[0]);
+
 });
 
 module.exports = router;
